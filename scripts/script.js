0852d63bdc79a52ff2e01270b7047e9469d45647
Jorin Slaybaugh <jslaybaugh@gmail.com>
2013-01-19 15:02:58 -0600

    coding done

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,8 @@ var obj = {};
 	var vm = {
 		Participants: ko.observableArray(),
 		Entries: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],
-		WheelExists: ko.observable()
+		WheelExists: ko.observable(),
+		Winner: ko.observable("")
 	};
 
 	vm.ParticipantCount = ko.computed(function() { return vm.Participants().length; });
@@ -37,9 +38,8 @@ var obj = {};
 	// set up wheel
 	/// large portions of this code taken from: http://www.switchonthecode.com/tutorials/creating-a-roulette-wheel-using-html5-canvas
 
-	var colors = ["#B8D430", "#3AB745", "#029990", "#3501CB",
-             "#2E2C75", "#673A7E", "#CC0071", "#F80120",
-             "#F35B20", "#FB9A00", "#FFCC00", "#FEF200"];
+	var colors = ["#3FB618", "#FF0039", "#FF7518", "#9954BB",
+             "#007FFF", "#73C9E3", "#EDE55A", "#BEADED"];
 
 	var startAngle = 0;
 	var arc = Math.PI / 6;
@@ -64,110 +64,113 @@ var obj = {};
 		if (containerWidth < 500) base = containerWidth;
 		mark = parseInt(base / 2);
 
-	  var canvas = document.getElementById("canvas");
-	  if (canvas.getContext) {
-	    var outsideRadius = parseInt(mark * 0.8);
-	    var textRadius = parseInt(outsideRadius * 0.8);
-	    var insideRadius = 75;
-	    
-	    ctx = canvas.getContext("2d");
-	    ctx.clearRect(0,0,canvasWidth,canvasHeight);
-	    
-	    
-	    ctx.strokeStyle = "white";
-	    ctx.lineWidth = 2;
-	    
-	    ctx.font = 'bold ' + (0.04 * base) + 'px Helvetica, Arial';
+		var canvas = document.getElementById("canvas");
+		if (canvas.getContext) {
+			var outsideRadius = parseInt(mark * 0.8);
+			var textRadius = parseInt(outsideRadius * 0.8);
+			var insideRadius = 25;
 
-	    wheelEntries = [];
-	    for (var i = 0; i < vm.Participants().length; i++) {
-	    	for (var j = 0; j < vm.Participants()[i].Entries; j++) {
-	    		wheelEntries.push(vm.Participants()[i].Name);
-	    	};
-	    };
-	    
-	    arc = Math.PI / (wheelEntries.length / 2);
+			ctx = canvas.getContext("2d");
+			ctx.clearRect(0,0,canvasWidth,canvasHeight);
 
-	    for(var i = 0; i < wheelEntries.length; i++) {
 
-	      var angle = startAngle + i * arc;
-	      ctx.fillStyle = colors[i];
-	      
-	      ctx.beginPath();
-	      ctx.arc(mark, mark, outsideRadius, angle, angle + arc, false);
-	      ctx.arc(mark, mark, insideRadius, angle + arc, angle, true);
-	      ctx.stroke();
-	      ctx.fill();
-	      
-	      ctx.save();
-	      //ctx.shadowOffsetX = -1;
-	      //ctx.shadowOffsetY = -1;
-	      //ctx.shadowBlur    = 0;
-	      //ctx.shadowColor   = "rgb(220,220,220)";
-	      ctx.fillStyle = "white";
-	      ctx.translate(mark + Math.cos(angle + arc / 2) * textRadius, 
-	                    mark + Math.sin(angle + arc / 2) * textRadius);
-	      ctx.rotate(angle + arc / 2 + Math.PI / 2);
-	      var text = wheelEntries[i];
-	      ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
-	      ctx.restore();
-	    } 
-	    
-	    //Arrow
-	    ctx.fillStyle = "black";
-	    ctx.beginPath();
-	    ctx.moveTo(mark - 4, mark - (outsideRadius + 5));
-	    ctx.lineTo(mark + 4, mark - (outsideRadius + 5));
-	    ctx.lineTo(mark + 4, mark - (outsideRadius - 5));
-	    ctx.lineTo(mark + 9, mark - (outsideRadius - 5));
-	    ctx.lineTo(mark + 0, mark - (outsideRadius - 13));
-	    ctx.lineTo(mark - 9, mark - (outsideRadius - 5));
-	    ctx.lineTo(mark - 4, mark - (outsideRadius - 5));
-	    ctx.lineTo(mark - 4, mark - (outsideRadius + 5));
-	    ctx.fill();
+			ctx.strokeStyle = "white";
+			ctx.lineWidth = 2;
 
-	    canvasWidth = containerWidth;
-	    canvasHeight = containerHeight;
+			ctx.font = 'bold ' + (0.04 * base) + 'px Helvetica, Arial';
 
-	    vm.WheelExists(true);
-	  }
+			wheelEntries = [];
+			for (var i = 0; i < vm.Participants().length; i++) {
+				for (var j = 0; j < vm.Participants()[i].Entries; j++) {
+					wheelEntries.push(vm.Participants()[i].Name);
+				};
+			};
+
+			//wheelEntries.sort(function() {return 0.5 - Math.random()}); // randomize sort
+
+			arc = Math.PI / (wheelEntries.length / 2);
+
+			for(var i = 0; i < wheelEntries.length; i++) {
+
+			  var angle = startAngle + i * arc;
+			  ctx.fillStyle = colors[(i > 7 ?  i % 8 : i)];
+			  
+			  ctx.beginPath();
+			  ctx.arc(mark, mark, outsideRadius, angle, angle + arc, false);
+			  ctx.arc(mark, mark, insideRadius, angle + arc, angle, true);
+			  ctx.stroke();
+			  ctx.fill();
+			  
+			  ctx.save();
+			  //ctx.shadowOffsetX = -1;
+			  //ctx.shadowOffsetY = -1;
+			  //ctx.shadowBlur    = 0;
+			  //ctx.shadowColor   = "rgb(220,220,220)";
+			  ctx.fillStyle = "white";
+			  ctx.translate(mark + Math.cos(angle + arc / 2) * textRadius, 
+			                mark + Math.sin(angle + arc / 2) * textRadius);
+			  ctx.rotate(angle + arc / 2 + Math.PI / 2);
+			  var text = wheelEntries[i];
+			  ctx.fillText(text, -ctx.measureText(text).width / 2, 0);
+			  ctx.restore();
+			} 
+
+			//Arrow
+			ctx.fillStyle = "black";
+			ctx.beginPath();
+			ctx.moveTo(mark - 4, mark - (outsideRadius + 5));
+			ctx.lineTo(mark + 4, mark - (outsideRadius + 5));
+			ctx.lineTo(mark + 4, mark - (outsideRadius - 5));
+			ctx.lineTo(mark + 9, mark - (outsideRadius - 5));
+			ctx.lineTo(mark + 0, mark - (outsideRadius - 13));
+			ctx.lineTo(mark - 9, mark - (outsideRadius - 5));
+			ctx.lineTo(mark - 4, mark - (outsideRadius - 5));
+			ctx.lineTo(mark - 4, mark - (outsideRadius + 5));
+			ctx.fill();
+
+			canvasWidth = containerWidth;
+			canvasHeight = containerHeight;
+
+			vm.WheelExists(true);
+		}
 	}
 	    
 	function spin() {
-	  spinAngleStart = Math.random() * 10 + 10;
-	  spinTime = 0;
-	  spinTimeTotal = Math.random() * 3 + 4 * 1000;
-	  rotateWheel();
+		vm.Winner("");
+		spinAngleStart = Math.random() * 10 + 10;
+		spinTime = 0;
+		spinTimeTotal = ((Math.random() * 20) + 4) * 1000;
+		rotateWheel();
 	}
 
 	function rotateWheel() {
-	  spinTime += 30;
-	  if(spinTime >= spinTimeTotal) {
-	    stopRotateWheel();
-	    return;
-	  }
-	  var spinAngle = spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
-	  startAngle += (spinAngle * Math.PI / 180);
-	  drawRouletteWheel();
-	  spinTimeout = setTimeout(function() { rotateWheel() }, 30);
+		spinTime += 30;
+		if(spinTime >= spinTimeTotal) {
+			stopRotateWheel();
+			return;
+		}
+		var spinAngle = spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
+		startAngle += (spinAngle * Math.PI / 180);
+		drawRouletteWheel();
+		spinTimeout = setTimeout(function() { rotateWheel() }, 30);
 	}
 
 	function stopRotateWheel() {
-	  clearTimeout(spinTimeout);
-	  var degrees = startAngle * 180 / Math.PI + 90;
-	  var arcd = arc * 180 / Math.PI;
-	  var index = Math.floor((360 - degrees % 360) / arcd);
-	  ctx.save();
-	  ctx.fillStyle = "black";
-	  ctx.font = 'bold 30px Helvetica, Arial';
-	  var text = wheelEntries[index]
-	  ctx.fillText(text, mark - ctx.measureText(text).width / 2, mark + 10);
-	  ctx.restore();
+		clearTimeout(spinTimeout);
+		var degrees = startAngle * 180 / Math.PI + 90;
+		var arcd = arc * 180 / Math.PI;
+		var index = Math.floor((360 - degrees % 360) / arcd);
+		ctx.save();
+		//ctx.fillStyle = "black";
+		//ctx.font = 'bold 30px Helvetica, Arial';
+		vm.Winner(wheelEntries[index] + " wins!");
+		//ctx.fillText(text, mark - ctx.measureText(text).width / 2, mark + 10);
+		//ctx.restore();
 	}
 
 	function easeOut(t, b, c, d) {
-	  var ts = (t/=d)*t;
-	  var tc = ts*t;
-	  return b+c*(tc + -3*ts + 3*t);
+		var ts = (t/=d)*t;
+		var tc = ts*t;
+		return b+c*(tc + -3*ts + 3*t);
 	}
 })()
